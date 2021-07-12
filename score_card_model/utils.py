bbdd75de9b8be02be357db82474d7bf0ce866bb3
skykiseki <595791833@qq.com>
2021-07-12 16:52:18 +0800

    Update utils.py
    add func chi2sum_id

--- score_card_model/utils.py
+++ score_card_model/utils.py
@@ -77,3 +77,61 @@ def chi2(regroup):
         chi2_good = ((sub_rg['num_feat_good'] - sub_rg['num_good_expected']) ** 2 / sub_rg['num_good_expected']).sum()
     # 返回总体的卡方值chi2_bad + chi2_good
     return chi2_bad + chi2_good
+
+
+
+def chi2sum_id(regroup, field='all'):
+    """
+    找出需要待合并的分箱位置(id)
+
+    Parameters:
+    ----------
+    regroup, 特征bad_rate的dataframe, *****但是这里的dataframe是经过排序的
+    field:
+    若field ='all', 则比较所有的相邻组, 可选的取值为[0, regroup.shape[0] - 1], 返回(id_min, id_min + 1)
+    若field = 0, 则仅能与index=1的组进行合并, 返回1
+    若field = regroup.shape[0] - 1,则仅能与index = regroup.shape[0] - 2的组进行合并
+    若field = k, 其中k属于[1, regroup.shape[0] - 1, 若k与k+1的卡方值更小, 则返回k+1, 若k与k-1的卡方值更小, 则返回k
+
+    Returns:
+    -------
+    id_min: 最小卡方值所在箱的索引 + 1
+    """
+    # 特征的取值数,即原始箱数
+    vals_cnt = regroup.shape[0]
+    # 判断field的输入是否正确
+    if isinstance(field, str):
+        if field != 'all':
+            print("Incorrect field para is inputted.The parameter 'field' should equal to 'all'.")
+    elif not isinstance(field, int):
+        print("Incorrect field para is inputted.The type of field para should be either 'str' or 'int'.")
+    else:
+        if field < 0 or field >= vals_cnt:
+            print("Incorrect field para is inputted.The range of fielf para should be from 0 to " + str(vals_cnt - 1))
+
+    if field == 'all':
+        # 遍历相邻两组的index,分别计算各自的卡方值
+        list_chi2 = []
+        for i in range(vals_cnt - 1):
+            # 注意这个地方,iloc不取结尾部分的index, 所以是加2
+            sub_regroup = regroup.iloc[i: i + 2]
+            # 计算子集的卡方值
+            chi2_subrg = chi2(sub_regroup)
+            list_chi2.append(chi2_subrg)
+        # 计算最小卡方值所在的索引, 返回+1值
+        id_minchi2 = list_chi2.index(min(list_chi2))
+        id_min = id_minchi2 + 1
+    elif field == vals_cnt - 1:
+        id_min = vals_cnt - 1
+    elif field == 0:
+        id_min = 1
+    else:
+        k = field
+        # 计算k-1和k+1处的卡方值
+        chi2_last = chi2(regroup.iloc[k - 1: k + 1])
+        chi2_next = chi2(regroup.iloc[k: k + 2])
+        if chi2_last <= chi2_next:
+            id_min = k
+        else:
+            id_min = k + 1
+    return id_min
