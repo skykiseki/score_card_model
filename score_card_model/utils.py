9958d67eb9ed85bb58b5a6f43e90ccb06f4eb7c1
skykiseki <595791833@qq.com>
2021-07-12 16:51:29 +0800

    Update utils.py
    add func chi2

--- score_card_model/utils.py
+++ score_card_model/utils.py
@@ -25,3 +25,55 @@ def bin_badrate(df, col_name, target):
     # 计算该特征的bad_rate
     redf['bad_rate'] = redf['num_feat_bad'] / redf['num_feat_vals']
     return redf
+
+
+
+def chi2(regroup):
+    """
+    计算并返回两个相邻组的卡方值
+
+    当相邻两组组坏样本都为0（或1）时，over_badrate为0（或1），期望坏样本数为0（或1），实际与期望一样，卡方值为0；
+    计算regroup的卡方值chi2
+
+    ps:只适合用在二分类0,1问题
+    ps:注意这里是排序的regroup
+
+    Parameters:
+    ----------
+    regroup：输入的dataframe
+
+    Returns:
+    -------
+    卡方值
+    """
+    # 先copy,后期会有加计算字段
+    sub_rg = regroup.copy()
+    # 计算总频数n
+    num_total = sub_rg['num_feat_vals'].sum()
+
+    # 计算bad在各箱的频数
+    sub_rg['num_feat_good'] = sub_rg['num_feat_vals'] - sub_rg['num_feat_bad']
+    # 计算bad的频数
+    num_bad = sub_rg['num_feat_bad'].sum()
+    # 计算good的频数
+    num_good = sub_rg['num_feat_good'].sum()
+    # 计算bad(class='1')的期望频率
+    overall_badrate = num_bad / num_total
+    # 计算good(class = '0')的期望频率
+    overall_goodrate = num_good / num_total
+    # 计算各个箱bad的期望频率
+    sub_rg['num_bad_expected'] = sub_rg['num_feat_vals'] * overall_badrate
+    # 计算各个箱good的期望频率
+    sub_rg['num_good_expected'] = sub_rg['num_feat_vals'] * overall_goodrate
+    # 计算bad下的卡方值chi2, 如果bad_rate为0, 卡方值也为0
+    if overall_badrate == 0:
+        chi2_bad = 0
+    else:
+        chi2_bad = ((sub_rg['num_feat_bad'] - sub_rg['num_bad_expected']) ** 2 / sub_rg['num_bad_expected']).sum()
+    # 计算good下的卡方值chi2, 如果good_rate为0, 卡方值也为0
+    if overall_goodrate == 0:
+        chi2_good = 0
+    else:
+        chi2_good = ((sub_rg['num_feat_good'] - sub_rg['num_good_expected']) ** 2 / sub_rg['num_good_expected']).sum()
+    # 返回总体的卡方值chi2_bad + chi2_good
+    return chi2_bad + chi2_good
