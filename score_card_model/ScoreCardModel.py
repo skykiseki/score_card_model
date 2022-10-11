77fd2533bef0465d540085b1ec5125018764803d
skykiseki <595791833@qq.com>
2022-10-11 11:33:17 +0800

    update v1.6.1
    优化了filter_corr的逻辑

--- score_card_model/ScoreCardModel.py
+++ score_card_model/ScoreCardModel.py
@@ -618,12 +618,9 @@ class ScoreCardModel(object):
         cols_filter = set()
         dict_feat_corr = {}
 
-        # 剔除target
-        df = df_woe.drop(self.target, axis=1)
-
         # 计算相关系数, 记得剔除feat本身组合
         # 注意这里含(A, B) 和(B, A）的重复组合
-        corr_feat = df.corr()
+        corr_feat = df_woe.loc[:, self.cols].corr()
 
         for col in corr_feat.columns:
             for idx in corr_feat.index:
